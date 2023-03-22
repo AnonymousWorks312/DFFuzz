885c981ed380b64f00dbc870b2c4819bc96383e7
AnonymousWorks312 <128578462+AnonymousWorks312@users.noreply.github.com>
2023-03-22 16:54:16 +0800

    Add files via upload

--- experiment_builder.py
+++ experiment_builder.py
@@ -85,10 +85,6 @@ def _get_model_v2(params, experiment):
         model = keras.models.load_model('models/mnist_lenet5_advtrain/adv_train/keras_mnist_lenet5_cw_0.9830.h5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         # model.summary()
-    elif params.model == 'LeNet5_adv_bim':
-        model = keras.models.load_model('models/mnist_lenet5_advtrain/adv_train/keras_mnist_lenet5_bim_0.9750.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        # model.summary()
     elif params.model == "vgg16":
         model = keras.models.load_model(
             'models/cifar10_vgg16_0.8tr/train_on_0.2tr/keras_cifar10_vgg16_model.017-0.8788.h5')
@@ -96,10 +92,6 @@ def _get_model_v2(params, experiment):
         # model.summary()
     elif params.model == "vgg16_adv_cw":
         model = keras.models.load_model('models/cifar10_vgg16_advtrain/adv_train/keras_cifar10_vgg16_cw_0.8800.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        # model.summary()
-    elif params.model == 'vgg16_adv_bim':
-        model = keras.models.load_model('models/cifar10_vgg16_advtrain/adv_train/keras_cifar10_vgg16_bim_0.8751.h5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         # model.summary()
     elif params.model == "resnet18":
@@ -110,99 +102,12 @@ def _get_model_v2(params, experiment):
         model = keras.models.load_model(
             'models/svhn_resnet18_advtrain/adv_train/keras_svhn_resnet18_cw_model.002-0.9201_v2.h5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "resnet18_adv_bim":
-        model = keras.models.load_model(
-            'models/svhn_resnet18_advtrain/adv_train/keras_svhn_resnet18_bim_model.005-0.9190_v2.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
     elif params.model == "Alexnet":
         model = keras.models.load_model('models/fm_alexnet_0.8tr/train_on_0.2tr/keras_fm_alexnet_model.016-0.9034.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "Alexnet_adv_bim":
-        model = keras.models.load_model('models/fm_alexnet_advtrain/adv_train/keras_fm_alexnet_bim.034-0.9096_v2.h5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     elif params.model == "Alexnet_adv_cw":
         model = keras.models.load_model('models/fm_alexnet_advtrain/adv_train/keras_fm_alexnet_cw.002-0.9187_v2.h5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "LeNet5_quant":
-        import tensorflow as tf
-        model = tf.lite.Interpreter(
-            model_path="models/mnist_lenet5_advtrain/quantization/mnist_lenet5_converted.tflite")
-    elif params.model == "LeNet5_prune":
-        import tensorflow
-        model = tensorflow.keras.models.load_model('models/mnist_lenet5_advtrain/prune/mnist_prune_0.9812.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "vgg16_quant":
-        import tensorflow as tf
-        model = tf.lite.Interpreter(
-            model_path="models/cifar10_vgg16_advtrain/quantization/cifar10_vgg16_converted_int8.tflite")
-    elif params.model == "Alexnet_quant":
-        import tensorflow as tf
-        model = tf.lite.Interpreter(
-            model_path="models/fm_alexnet_advtrain/quantization/fm_alexnet_converted_int8.tflite")
-    elif params.model == "resnet18_quant":
-        import tensorflow as tf
-        model = tf.lite.Interpreter(
-            model_path="models/svhn_resnet18_advtrain/quantization/svhn_resnet18_converted_int8.tflite")
-    elif params.model == "vgg16_prune":
-        import tensorflow
-        model = tensorflow.keras.models.load_model('models/cifar10_vgg16_advtrain/prune/cifar10_prune_0.7627.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "Alexnet_prune":
-        import tensorflow
-        model = tensorflow.keras.models.load_model('models/fm_alexnet_advtrain/prune/fm_prune_0.9154.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "resnet18_prune":
-        import tensorflow
-        model = tensorflow.keras.models.load_model('models/svhn_resnet18_advtrain/prune/svhn_prune_0.9100.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "Alexnet_apricot":
-        model = keras.models.load_model('models/fm_alexnet_advtrain/apricot/fashion_mnist_apricot_0.929.hdf5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "LeNet5_apricot":
-        model = keras.models.load_model('models/mnist_lenet5_advtrain/apricot/mnist_apricot_0.9812.hdf5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "resnet18_apricot":
-        model = keras.models.load_model('models/svhn_resnet18_advtrain/apricot/svhn_apricot_0.921.hdf5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "vgg16_apricot":
-        model = keras.models.load_model('models/cifar10_vgg16_advtrain/apricot/cifar10_apricot_0.884.hdf5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "rq3_LeNet5":
-        model = keras.models.load_model(
-            'models/mnist_lenet5_0.8tr/train_on_0.2tr/keras_mnist_lenet5_model.014-0.9783.h5')
-        model.load_weights('rq3_m2prime/rq3_mnist_lenet5_tr/model.011-0.9926_on_deeprefuzz.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "rq3_LeNet5_onv1":
-        model = keras.models.load_model(
-            'models/mnist_lenet5_0.8tr/train_on_0.2tr/keras_mnist_lenet5_model.014-0.9783.h5')
-        model.load_weights('rq3_m2prime/rq3_mnist_lenet5_tr/model.016-0.9775-ondeeprefuzz_onv1.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "rq3_vgg16":
-        model = keras.models.load_model(
-            'models/cifar10_vgg16_0.8tr/train_on_0.2tr/keras_cifar10_vgg16_model.017-0.8788.h5')
-        model.load_weights('rq3_m2prime/rq3_cifar10_vgg16_tr/model.020-0.8841-ondeeprefuzz.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "rq3_vgg16_onv1":
-        model = keras.models.load_model(
-            'models/cifar10_vgg16_0.8tr/train_on_0.2tr/keras_cifar10_vgg16_model.017-0.8788.h5')
-        model.load_weights('rq3_m2prime/rq3_cifar10_vgg16_tr/model.007-0.8846-ondeeprefuzz_onv1.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "rq3_Alexnet":
-        model = keras.models.load_model('models/fm_alexnet_0.8tr/train_on_0.2tr/keras_fm_alexnet_model.016-0.9034.h5')
-        model.load_weights('rq3_m2prime/rq3_fm_alexnet_tr/model.020-0.9165-ondeeprefuzz.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "rq3_Alexnet_onv1":
-        model = keras.models.load_model('models/fm_alexnet_0.8tr/train_on_0.2tr/keras_fm_alexnet_model.016-0.9034.h5')
-        model.load_weights('rq3_m2prime/rq3_fm_alexnet_tr/model.011-0.9105-ondeeprefuzz_onv1.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "rq3_resnet18":
-        model = keras.models.load_model(
-            'models/svhn_resnet18_0.8tr/train_on_0.2tr/keras_svhn_resnet18_model.003-0.9193.h5')
-        model.load_weights('rq3_m2prime/rq3_svhn_resnet18_tr/model.030-0.9140-ondeeprefuzz.h5')
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
     else:
         raise Exception("Unknown Model:" + str(params.model))
 
@@ -212,41 +117,41 @@ def _get_model_v2(params, experiment):
 def _get_model(params, experiment):
     import os
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-    if params.model == "LeNet5" or params.model == "rq3_LeNet5" or params.model == "rq3_LeNet5_onv1":
+    if params.model == "LeNet5":
         # import tensorflow as tf
         model = keras.models.load_model(
             'models/mnist_lenet5_0.8tr/keras_Fri-May-28-06-01-34-2021.model.008-0.8587.hdf5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         # model.summary()
-    elif params.model == "LeNet5_adv_cw" or params.model == "LeNet5_adv_bim" or params.model == "LeNet5_quant" or params.model == "LeNet5_prune" or params.model == "LeNet5_apricot":
+    elif params.model == "LeNet5_adv_cw":
         # import tensorflow as tf
         model = keras.models.load_model(
             'models/mnist_lenet5_advtrain/keras_Mon-Dec-27-09-39-09-2021.model.011-0.9807.hdf5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         # model.summary()
-    elif params.model == "vgg16" or params.model == "rq3_vgg16" or params.model == "rq3_vgg16_onv1":
+    elif params.model == "vgg16":
         # import tensorflow as tf
         model = keras.models.load_model(
             'models/cifar10_vgg16_0.8tr/keras_Sat-Oct-30-01-36-17-2021.model.094-0.8767.hdf5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         # model.summary()
-    elif params.model == "vgg16_adv_cw" or params.model == "vgg16_adv_bim" or params.model == "vgg16_quant" or params.model == "vgg16_prune" or params.model == "vgg16_apricot":
+    elif params.model == "vgg16_adv_cw":
         # import tensorflow as tf
         model = keras.models.load_model(
             'models/cifar10_vgg16_advtrain/keras_Wed-Mar-24-17-55-44-2021.model.135-0.8792.hdf5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         # model.summary()
-    elif params.model == "resnet18" or params.model == "rq3_resnet18":
+    elif params.model == "resnet18":
         model = keras.models.load_model(
             'models/svhn_resnet18_0.8tr/keras_Tue-Dec-28-20-09-09-2021.model.004-0.8885_v1.hdf5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "resnet18_adv_cw" or params.model == "resnet18_adv_bim" or params.model == "resnet18_quant" or params.model == "resnet18_prune" or params.model == "resnet18_apricot":
+    elif params.model == "resnet18_adv_cw":
         model = keras.models.load_model('models/svhn_resnet18_advtrain/keras_svhn_resnet18_model.006-0.9205_v1.hdf5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "Alexnet" or params.model == "rq3_Alexnet" or params.model == "rq3_Alexnet_onv1":
+    elif params.model == "Alexnet":
         model = keras.models.load_model('models/fm_alexnet_0.8tr/keras_fm_alexnet.019-0.8933_v1.hdf5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    elif params.model == "Alexnet_adv_cw" or params.model == "Alexnet_adv_bim" or params.model == "Alexnet_quant" or params.model == "Alexnet_prune" or params.model == "Alexnet_apricot":
+    elif params.model == "Alexnet_adv_cw":
         model = keras.models.load_model('models/fm_alexnet_advtrain/keras_fm_alexnet.098-0.9170_v1.hdf5')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
